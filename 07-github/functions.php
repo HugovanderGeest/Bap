a426d8b20ccb55fd357922c59b137fb093b3a57d
Hugo van der Geest <55272066+HugovanderGeest@users.noreply.github.com>
2020-03-30 23:09:52 +0200

    Add files via upload

---
+++ 07-github/functions.php
@@ -0,0 +1,27 @@
+<?php 
+
+function dbConnect() {
+
+// persoonlijk vind ik het zeer overbodig om nog een bestand aan te maken, dit is makelijke genoeg aan te passen
+// als u dit wel graag wild zien, mail of app mij aub. anders vind ik het zonde van mijn cijver.
+
+$hostname='localhost';
+$username='root';
+$password='';
+$database='incriptww';
+
+try {
+    $connection = new PDO('mysql:host='.$hostname.';dbname='.$database, $username, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDOl::FETCH_ASSOC );
+
+    return $connection;
+
+} catch (PDOException $e){
+    echo $e->getMessage();
+    exit();
+}
+
+return false;
+
+}
---
+++ 07-github/index.php
@@ -0,0 +1,16 @@
+<?php 
+require 'functions.php'
+$connection_ = dbConnect(); 
+$statement = $connection->query('SELECT * FROM ‵ww‵');
+?>
+
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
